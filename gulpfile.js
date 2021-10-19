ed081f19ac12f391e9026e178d25571930c2cc99
David Agaybi <davidagaybi@gmail.com>
2021-10-19 05:51:55 -0400

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("davosion/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("davosion/**/*.scss").pipe(sass()).pipe(purgecss({content: ['*.html']})).pipe(dest("css"));
 }
 
 function watchTask() {
